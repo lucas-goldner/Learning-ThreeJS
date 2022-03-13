93ed748c3116894907119fef0c7f4ed310279c94
lucas-goldner <lucas.goldner@googlemail.com>
2022-03-13 01:00:44 +0100

    feat: Add an entire group of objects into the scene

--- lesson4_5/src/script.js
+++ lesson4_5/src/script.js
@@ -4,19 +4,46 @@ import * as THREE from "three";
 const scene = new THREE.Scene();
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const material = new THREE.MeshBasicMaterial({ color: 0x000ff0 });
 const mesh = new THREE.Mesh(geometry, material);
 
 //Playing with transform properties
 mesh.position.x = 0.7;
 mesh.position.y = -0.6;
-mesh.position.z = -1;
+mesh.position.z = 1;
 mesh.scale.x = 2;
 mesh.scale.y = 0.2;
 mesh.rotation.x = Math.PI * 0.2;
 mesh.rotation.y = Math.PI * 0.25;
 
 scene.add(mesh);
+
+//Group
+const group = new THREE.Group();
+group.scale.y = 2;
+group.rotation.y = 0.2;
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+cube1.position.x = -1.5;
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+cube2.position.x = 0;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+cube3.position.x = 1.5;
+group.add(cube3);
 
 const sizes = {
   width: 800,
@@ -25,7 +52,7 @@ const sizes = {
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
-camera.lookAt(mesh.position);
+camera.lookAt(group.position);
 scene.add(camera);
 
 const axes = new THREE.AxesHelper(2);
