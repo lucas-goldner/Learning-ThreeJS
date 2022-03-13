8646c4ddfae5bdcf0a5f238a0a269fa0e4fde693
lucas-goldner <lucas.goldner@googlemail.com>
2022-03-14 00:02:32 +0100

    feat: Try out orthographic camera

--- lesson4Plus/src/script_6.js
+++ lesson4Plus/src/script_6.js
@@ -7,7 +7,7 @@ const scene = new THREE.Scene();
 
 //Object
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
@@ -16,7 +16,22 @@ const sizes = {
   width: window.innerWidth * 0.9,
   height: window.innerHeight * 0.9,
 };
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   sizes.width / sizes.height,
+//   1,
+//   100
+// );
+const aspectRatio = sizes.width / sizes.height;
+const camera = new THREE.OrthographicCamera(
+  -1 * aspectRatio,
+  1 * aspectRatio,
+  1,
+  -1,
+  0.1,
+  100
+);
+
 camera.position.z = 3;
 scene.add(camera);
 
