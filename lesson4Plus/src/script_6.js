c7fc6368644d5aaa6946e3548ede4eb3e132bb5a
lucas-goldner <lucas.goldner@googlemail.com>
2022-03-14 22:59:53 +0100

    feat: Make canvas reponsive

--- lesson4Plus/src/script_6.js
+++ lesson4Plus/src/script_6.js
@@ -70,3 +70,11 @@ const tick = () => {
 };
 
 tick();
+
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+});
