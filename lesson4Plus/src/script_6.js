b0b34ce6c0ec5efae4400050e30ac267195c84d8
lucas-goldner <lucas.goldner@googlemail.com>
2022-03-15 00:14:27 +0100

    feat: Add specific webkit functions for safaris fullscreen

--- lesson4Plus/src/script_6.js
+++ lesson4Plus/src/script_6.js
@@ -81,11 +81,20 @@ window.addEventListener("resize", () => {
 });
 
 window.addEventListener("dblclick", () => {
-  if (!document.fullscreenElement) {
-    console.log("go fullscreen");
-    canvas.requestFullscreen();
+  const fullscreenElement =
+    document.fullscreenElement || document.webkitFullscreenElement;
+
+  if (!fullscreenElement) {
+    if (canvas.requestFullscreen) {
+      canvas.requestFullscreen();
+    } else if (canvas.webkitRequestFullscreen) {
+      canvas.webkitRequestFullscreen();
+    }
   } else {
-    console.log("leave fullscreen");
-    document.exitFullscreen();
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
   }
 });
