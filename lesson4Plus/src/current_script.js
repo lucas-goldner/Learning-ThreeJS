d7a586f2c4b5311cc439dd06b301095141795e39
lucas-goldner <lucas.goldner@googlemail.com>
2022-03-15 22:57:51 +0100

    refactor: Into current_script to be default loaded

---
+++ lesson4Plus/src/current_script.js
@@ -0,0 +1,80 @@
+import "./style.css";
+import * as THREE from "three";
+import gsap from "gsap";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+//Scene
+const scene = new THREE.Scene();
+
+//Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+const mesh = new THREE.Mesh(geometry, material);
+mesh.rotateX(2);
+mesh.rotateZ(2);
+scene.add(mesh);
+
+//Camera
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  1,
+  100
+);
+
+camera.position.z = 3;
+camera.lookAt(mesh.position);
+scene.add(camera);
+
+//Renderer
+const canvas = document.querySelector("canvas.webgl");
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+
+//Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+const tick = () => {
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+  controls.update();
+  camera.lookAt(mesh.position);
+};
+
+tick();
+
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+window.addEventListener("dblclick", () => {
+  const fullscreenElement =
+    document.fullscreenElement || document.webkitFullscreenElement;
+
+  if (!fullscreenElement) {
+    if (canvas.requestFullscreen) {
+      canvas.requestFullscreen();
+    } else if (canvas.webkitRequestFullscreen) {
+      canvas.webkitRequestFullscreen();
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+});
